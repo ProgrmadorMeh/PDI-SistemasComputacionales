f417fb3ff61b5dd28c4d0f0c0a14be832e7b50fc
ProgrmadorMeh <168498404+ProgrmadorMeh@users.noreply.github.com>
2025-08-05 19:57:11 -0300

    Update index.js

--- tp-10/index.js
+++ tp-10/index.js
@@ -4,11 +4,11 @@ import tareasRouter from "./routes/tareas.routes.js";
 const app = express();
 const port = 3000;
 
-app.use(express.json()); // Para poder recibir JSON en req.body
+app.use(express.json());
 
-// Usa el router con un prefijo /api (puedes cambiarlo)
 app.use("/trabajo", tareasRouter);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
